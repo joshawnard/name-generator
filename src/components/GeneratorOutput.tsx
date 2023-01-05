1df13ab5484b62ac0eadc67d53e34d9dd89827c6
Joshua Ard <ard.josh@gmail.com>
2023-01-05 10:14:24 -0500

    cleanup

--- src/components/GeneratorOutput.tsx
+++ src/components/GeneratorOutput.tsx
@@ -2,6 +2,9 @@ import React, { useContext } from 'react';
 import NameGeneratorContext from "../NameGeneratorContext";
 import roots from "../root_words";
 
+// TODO:
+// Add randomizer that constructs multiple roots together
+
 const GeneratorOutput = () => {
   const { selectedWords } = useContext(NameGeneratorContext);
 
@@ -9,25 +12,24 @@ const GeneratorOutput = () => {
     <div>
       {
         Object.entries(selectedWords).map((selectedWord) => {
-          const type = selectedWord[0];
-          const list = selectedWord[1];
-
-          console.log(list);
+          const type = selectedWord[0]; // kinship
+          const list = selectedWord[1]; // ["mother"]
 
           if (list.length) {
             return (
               <>
                 {
                   list.map((word) => {
+                    // TODO: Make sense of all this; maybe I don't even need it
                     if (roots[type]) {
                       return Object.entries(roots[type]).map((wordName) => {
-                        const engWord = wordName[0];
+                        // const engWord = wordName[0];
                         const translationObj = wordName[1];
 
                         return Object.entries(translationObj).map((langWord) => {
                           return (
                             <div key={langWord[0]}>
-                              <h3>{engWord}</h3>
+                              <h3>{word}</h3>
 
                               {
                                 Object.entries(langWord[1]).map((foo) => {
@@ -37,7 +39,7 @@ const GeneratorOutput = () => {
                                       {
                                         foo[1] && foo[1].length && (
                                           <div>
-                                            {foo[1][0]}
+                                            {foo[1][0]}, {foo[0]}
                                           </div>
                                         )
                                       }
